6d248bc9967d1585e98450666806450ca5bb7e80
donghao08 <donghao08@baidu.com>
2021-09-20 21:27:14 +0800

    实现Python中的eval()

--- src/main/java/StringExp.java
+++ src/main/java/StringExp.java
@@ -1,7 +1,9 @@
 import org.apache.commons.jexl3.JexlBuilder;
 import org.apache.commons.jexl3.JexlEngine;
 import org.apache.commons.jexl3.JexlExpression;
-
+/*
+实现Python中的eval()
+ */
 public class StringExp {
     public static void main(String[] args) {
         /**
@@ -12,7 +14,7 @@ public class StringExp {
          <version>3.1</version>
          </dependency>
          */
-        String expressionString = "(2+3)*4";
+        String expressionString = "((100 + 3)*4 + 12/4)/5";
         JexlEngine jexlEngine = new JexlBuilder().create();
         JexlExpression jexlExpression = jexlEngine.createExpression(expressionString);
         Object evaluate = jexlExpression.evaluate(null);
